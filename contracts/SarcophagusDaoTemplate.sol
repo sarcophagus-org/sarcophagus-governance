984c4c62d61b88d230804f866d110d10ac89c27e
Adam Gall <adam@decentlabs.io>
2021-02-25 14:19:15 -0500

    Simplify the Template

--- contracts/SarcophagusDaoTemplate.sol
+++ contracts/SarcophagusDaoTemplate.sol
@@ -3,12 +3,6 @@ pragma solidity 0.4.24;
 import "@aragon/templates-shared/contracts/BaseTemplate.sol";
 
 contract SarcophagusDaoTemplate is BaseTemplate {
-    string private constant ERROR_BAD_VOTE_SETTINGS =
-        "SARCOPHAGUS_DAO_BAD_VOTE_SETTINGS";
-
-    address private constant ANY_ENTITY = address(-1);
-    uint64 private constant DEFAULT_FINANCE_PERIOD = uint64(30 days);
-
     event SarcophagusDaoDeployed(
         address dao,
         address acl,
@@ -38,27 +32,25 @@ contract SarcophagusDaoTemplate is BaseTemplate {
         MiniMeToken _sarcoVotingRights,
         uint64[3] memory _votingSettings
     ) public {
-        require(
-            _sarcoVotingRights != address(0),
-            "Invalid SARCO Voting Rights"
-        );
+        require(_sarcoVotingRights != address(0), "Invalid SARCO Voting Rights");
+        require(_votingSettings.length == 3, "Invalid voting settings");
 
         _validateId(_id);
-        _validateVotingSettings(_votingSettings);
 
         (Kernel dao, ACL acl) = _createDAO();
-        (Voting voting, Agent agent, Finance finance) =
-            _setupApps(
-                dao,
-                acl,
-                _sarcoVotingRights,
-                _votingSettings
-            );
+        Agent agent = _installDefaultAgentApp(dao);
+        Finance finance = _installFinanceApp(dao, agent, uint64(30 days));
+        Voting voting = _installVotingApp(dao, _sarcoVotingRights, _votingSettings);
+
+        _createAgentPermissions(acl, agent, voting, voting);
+        _createVaultPermissions(acl, Vault(agent), finance, voting);
+        _createFinancePermissions(acl, finance, voting, voting);
+        _createFinanceCreatePaymentsPermission(acl, finance, voting, address(this));
+        _createEvmScriptsRegistryPermissions(acl, voting, voting);
+        _createVotingPermissions(acl, voting, voting, address(-1), voting);
+
         _transferCreatePaymentManagerFromTemplate(acl, finance, voting);
-        _transferRootPermissionsFromTemplateAndFinalizeDAO(
-            dao,
-            voting
-        );
+        _transferRootPermissionsFromTemplateAndFinalizeDAO(dao, voting);
         _registerID(_id, dao);
 
         emit SarcophagusDaoDeployed(
@@ -69,55 +61,5 @@ contract SarcophagusDaoTemplate is BaseTemplate {
             address(agent),
             address(finance)
         );
-    }
-
-    function _setupApps(
-        Kernel _dao,
-        ACL _acl,
-        MiniMeToken _sarcoVotingRights,
-        uint64[3] memory _votingSettings
-    )
-        internal
-        returns (
-            Voting,
-            Agent,
-            Finance
-        )
-    {
-        Agent agent = _installDefaultAgentApp(_dao);
-        Finance finance =
-            _installFinanceApp(_dao, agent, DEFAULT_FINANCE_PERIOD);
-        Voting voting =
-            _installVotingApp(_dao, _sarcoVotingRights, _votingSettings);
-
-        _setupPermissions(_acl, agent, voting, finance);
-
-        return (voting, agent, finance);
-    }
-
-    function _setupPermissions(
-        ACL _acl,
-        Agent _agent,
-        Voting _voting,
-        Finance _finance
-    ) internal {
-        _createAgentPermissions(_acl, _agent, _voting, _voting);
-        _createVaultPermissions(_acl, Vault(_agent), _finance, _voting);
-        _createFinancePermissions(_acl, _finance, _voting, _voting);
-        _createFinanceCreatePaymentsPermission(
-            _acl,
-            _finance,
-            _voting,
-            address(this)
-        );
-        _createEvmScriptsRegistryPermissions(_acl, _voting, _voting);
-        _createVotingPermissions(_acl, _voting, _voting, ANY_ENTITY, _voting);
-    }
-
-    function _validateVotingSettings(uint64[3] memory _votingSettings)
-        private
-        pure
-    {
-        require(_votingSettings.length == 3, ERROR_BAD_VOTE_SETTINGS);
     }
 }
